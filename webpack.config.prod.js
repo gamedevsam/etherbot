75887bb8277af6b386004e6527de22af69f65726
Samuel Batista <sambatista1@gmail.com>
2018-07-08 23:51:57 -0400

    Chunking distribution files so they always get updated on the browser

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const ChunkHashReplacePlugin  = require('chunkhash-replace-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/main.jsx'),
   output: {
     path: path.resolve(__dirname, 'docs'),
-    filename: 'etherbot.js',
+    filename: 'etherbot.[chunkhash].js',
     publicPath: 'docs/',
     library: 'EtherBot',
     libraryTarget: 'umd',
@@ -14,8 +16,13 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   plugins: [
+    new CleanWebpackPlugin(['docs/etherbot*.js']),
     new UglifyJsPlugin({
       comments: false,
+    }),
+    new ChunkHashReplacePlugin({
+      src: 'src/index.html',
+      dest: 'docs/index.html',
     }),
   ],
   module: {
